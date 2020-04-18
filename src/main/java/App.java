c09958025e5b05a0b76cf55e7b87035ed79edd1b
Cheolwoo Nam <545i@naver.com>
2020-04-18 14:32:02 +0900

    2nd commit redis test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,8 +13,24 @@ public class App {
 		JedisPool pool = new JedisPool(jedisPoolConfig, ip, port, timeout, null);
 
 		Jedis jedis = pool.getResource();
+
+		// simple string
 		jedis.set("NAME", "Cheolwoo Nam");
 		System.out.println(jedis.get("NAME"));
+
+		// set
+		jedis.sadd("myset", "element1", "element2", "element3");
+		System.out.println("element2 is a member : " + jedis.sismember("myset", "element2"));
+
+		// list
+		jedis.rpush("mylist", "element1", "element2", "element3");
+		System.out.println("element at index 1 : " + jedis.lindex("mylist", 1));
+
+		// hash
+		jedis.hset("myhash", "word1", Integer.toString(2));
+		jedis.hincrBy("myhash", "word2", 1);
+		System.out.println("frequency or word1 : " + jedis.hget("myhash", "word1"));
+		System.out.println("frequency or word2 : " + jedis.hget("myhash", "word2"));
 
 		if (jedis != null) {
 			jedis.close();
